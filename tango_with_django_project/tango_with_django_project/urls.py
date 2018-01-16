cac39774b10d8967dd775acca2af220d92afccb4
ViktorSoftDev <32965939+viktorSoftDev@users.noreply.github.com>
2018-01-16 16:29:14 +0000

    added view and url maping. currently at page 25 in TWD

--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from rango import views
 
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^rango/', include('rango.urls')),
+    # above maps any URLs starting
+    # with rango/ to be handled by
+    # the rango application
     url(r'^admin/', admin.site.urls),
 ]
