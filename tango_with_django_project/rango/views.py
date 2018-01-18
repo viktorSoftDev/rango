93ca5101d89adf2e0718d9641068f6a610132550
ViktorSoftDev <32965939+viktorSoftDev@users.noreply.github.com>
2018-01-18 22:56:45 +0000

    added second view, now at chapter 4

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,8 +1,9 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hi there partner!")
+    return HttpResponse("Rango says hi there partner! <br/> <a href='/rango/about'>About</a>" )
 
-    
+def about(request):
+    return HttpResponse("Rango says here is the about page <br/> <a href='/rango/index'>Home</a>")
