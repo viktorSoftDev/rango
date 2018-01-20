907a9bfe31e65248bd4ad3637c319292e52b91ce
ViktorSoftDev <32965939+viktorSoftDev@users.noreply.github.com>
2018-01-20 17:07:56 +0000

    About page also modified to use template. serving media file (cat image) added and works. Now on chapter 5

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -12,6 +12,7 @@ def index(request):
     # Note that teh first parameter is the template we wish to use
     return render(request, 'rango/index.html', context=context_dict)
 
-    
+
 def about(request):
-    return HttpResponse("Rango says here is the about page <br/> <a href='/rango/index'>Home</a>")
+    context_dict = {'mymessage': "This tutorial has been put together by Viktor Eriksson"}
+    return render(request, 'rango/about.html', context=context_dict)
