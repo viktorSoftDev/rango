bc54d52a1e22a07d1a6284a7aa9fcfb095d39d29
ViktorSoftDev <32965939+viktorSoftDev@users.noreply.github.com>
2018-01-20 12:40:14 +0000

    Added templates and media service for index.html next: add template for about.html!

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hi there partner! <br/> <a href='/rango/about'>About</a>" )
+##### Keeping these comments as a note to myself what's going on..
+    # Construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
 
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier
+    # Note that teh first parameter is the template we wish to use
+    return render(request, 'rango/index.html', context=context_dict)
+
+    
 def about(request):
     return HttpResponse("Rango says here is the about page <br/> <a href='/rango/index'>Home</a>")
--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from rango import views
 
@@ -24,4 +26,4 @@ urlpatterns = [
     # with rango/ to be handled by
     # the rango application
     url(r'^admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
