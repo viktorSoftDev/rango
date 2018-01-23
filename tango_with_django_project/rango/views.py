a784f3ceed88a8a10c5954edf4287a1d6f6fc47e
ViktorSoftDev <32965939+viktorSoftDev@users.noreply.github.com>
2018-01-23 14:07:47 +0000

    time for parameterise URL mapping

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category, Page
+def show_category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context_dict)
 
 def index(request):
 ##### Keeping these comments as a note to myself what's going on..
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
 
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier
